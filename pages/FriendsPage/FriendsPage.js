b2bd2ab200c0a50da4674b86aa09cfb7e143b30d
Islam Youssief <Islam.youssief@gmail.com>
2019-03-24 18:54:39 +0200

    Sign Up Using Gmail

---
+++ pages/FriendsPage/FriendsPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const friendsPage = () => {
+  return (
+    <main>
+      <h1>Friends Page</h1>
+    </main>
+  );
+};
+
+export default friendsPage;
---
+++ pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const homepage = () => {
+  return (
+    <main>
+      <h1>Home Page</h1>
+    </main>
+  );
+};
+
+export default homepage;
