b94995b9c6d6631c02e6f38cd5ddc4363902fbfb
mostafa <mostapha.a.aly@gmail.com>
2019-03-28 11:14:29 +0200

    fix order_id

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import Navbar from "./components/Navbar/Navbar";
 
 //! pages
 import HomePage from "./pages/HomePage/HomePage";
-// import FriendsPage from "./pages/FriendsPage/FriendsPage";
-// import GroupsPage from "./pages/GroupsPage/GroupsPage";
-// import OrdersPage from "./pages/OrdersPage/OrdersPage";
+import FriendsPage from "./pages/FriendsPage/FriendsPage";
+import GroupsPage from "./pages/GroupsPage/GroupsPage";
+import OrdersPage from "./pages/OrdersPage/OrdersPage";
 import AddOrderPage from "./pages/AddOrderPage/AddOrderPage";
-// import OrderDetailsPage from "./pages/OrderDetailsPage/OrderDetailsPage";
+import OrderDetailsPage from "./pages/OrderDetailsPage/OrderDetailsPage";
 
 import Login from "./pages/LoginPage/LoginPage";
 import RegistrationForm from "./pages/RegisterPage/RegisterPage";
@@ -25,18 +25,20 @@ class App extends Component {
           <Navbar />
           <React.Fragment>
             <Switch>
-              <Route path="/login" exact component={Login} />
-              <Route path="/register" exact component={RegistrationForm} />
-              <Route path="/forgotPassword" exact component={ForgotPassword} />
-
               <Route path="/" exact component={HomePage} />
+              <Route path="/register" exact component={RegistrationForm} />
+              <Route path="/login" exact component={Login} />
+              <Route path="/forgotPassword" exact component={ForgotPassword} />
               {/* <Route path="/login" render={() => <LoginPage />} /> */}
               {/* <Route path="/register" render={() => <RegisterPage />} /> */}
-              {/*<Route path="/friends" component={FriendsPage} />*/}
-              {/*<Route path="/groups" component={GroupsPage} />*/}
-              {/*<Route path="/orders" render={() => <OrdersPage />} />*/}
-              {/*<Route path="/add-order" render={() => <AddOrderPage />} />*/}
-              {/*<Route path="/order-details" render={() => <OrderDetailsPage />}/>*/}
+              <Route path="/friends" component={FriendsPage} />
+              <Route path="/groups" component={GroupsPage} />
+              <Route path="/orders" render={() => <OrdersPage />} />
+              <Route path="/add-order" render={() => <AddOrderPage />} />
+              <Route
+                path="/order-details"
+                render={() => <OrderDetailsPage />}
+              />
             </Switch>
           </React.Fragment>
         </BrowserRouter>
